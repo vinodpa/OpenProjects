9dcae02f5136413c14a2f63f4e45199488a678d7
Vinod PA <vinodpa@kalycito.com>
2013-10-23 18:52:28 +0530

    [TASK] Updated the verification environment -- Changed the filelist to compile avalon files also

--- MyCodes/axi_ip/run/filelist.v
+++ MyCodes/axi_ip/run/filelist.v
@@ -1,3 +1,4 @@
-../rtl/axi_lite_master_v1_00_a/hdl/verilog/axi_lite_master.v
-../rtl/axi_lite_slave_v1_00_a/hdl/verilog/axi_lite_slave.v
+../rtl/pcores/axi_lite_master_v1_00_a/hdl/verilog/axi_lite_master.v
+../rtl/pcores/axi_lite_slave_v1_00_a/hdl/verilog/axi_lite_slave.v
+../../avalon_ip/rtl/avalon_slave/avalon_slave.v
 ../verification/tb_axi_lite.v
--- MyCodes/axi_ip/run/run.sh
+++ MyCodes/axi_ip/run/run.sh
@@ -1,4 +1,4 @@
-vsim -c tb_axi_lite 
-vsim -view wave
-vsim -add wave /* -r
-vsim run -all
+vsim -c tb_axi_lite
+run -all
+vcd2wlf mydump.vcd mydump.wlf
+quit
